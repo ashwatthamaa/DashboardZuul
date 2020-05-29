2ae2704b522d6c6d63f4f08d12f488f54710f89a
Bisht <P10504526@capita.co.uk>
2020-05-29 18:04:50 +0530

    Changes for fallback for case-management

--- src/main/java/com/example/demo/DashboardZuulApplication.java
+++ src/main/java/com/example/demo/DashboardZuulApplication.java
@@ -1,8 +1,18 @@
 package com.example.demo;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -11,5 +21,50 @@ public class DashboardZuulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DashboardZuulApplication.class, args);
 	}
+	
+	@Bean
+    public FallbackProvider caseManagementFallbackProvider() {
+        return new FallbackProvider() {
 
+            @Override
+            public String getRoute() {
+                return "case-management";
+            }
+
+            @Override
+            public ClientHttpResponse fallbackResponse(String route, Throwable throwable) {
+                return new ClientHttpResponse() {
+                    @Override
+                    public HttpStatus getStatusCode() throws IOException {
+                        return HttpStatus.OK;
+                    }
+
+                    @Override
+                    public int getRawStatusCode() throws IOException {
+                        return HttpStatus.OK.value();
+                    }
+
+                    @Override
+                    public String getStatusText() throws IOException {
+                        return HttpStatus.OK.toString();
+                    }
+
+                    @Override
+                    public void close() {}
+
+                    @Override
+                    public InputStream getBody() throws IOException {
+                        return new ByteArrayInputStream("{\"Error\":\"Case Management Service is down!\"}".getBytes());
+                    }
+
+                    @Override
+                    public HttpHeaders getHeaders() {
+                        HttpHeaders headers = new HttpHeaders();
+                        headers.setContentType(MediaType.APPLICATION_JSON);
+                        return headers;
+                    }
+                };
+            }
+        };
+    }
 }
